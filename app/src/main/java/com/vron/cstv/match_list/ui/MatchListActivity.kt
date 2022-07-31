0398d76e64ca7d13d35e9428e7c4e5b2390f2b5d
Victor Raft <victoraft@gmail.com>
2022-07-31 10:11:53 -0300

    Splash screen (#7)
    * Using SplashScreen API
    
    * Adding fuze vector
    
    * Fixing theme issue

--- app/src/main/java/com/vron/cstv/match_list/ui/MatchListActivity.kt
+++ app/src/main/java/com/vron/cstv/match_list/ui/MatchListActivity.kt
@@ -2,6 +2,7 @@ package com.vron.cstv.match_list.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.vron.cstv.R
@@ -12,11 +13,18 @@ import com.vron.cstv.match_list.presentation.MatchListViewModel
 import com.vron.cstv.match_list.presentation.ViewState
 import com.vron.cstv.match_list.ui.recycler.MarginItemDecoration
 import com.vron.cstv.match_list.ui.recycler.MatchListAdapter
+import kotlinx.coroutines.*
 import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.parameter.parametersOf
+import kotlin.coroutines.CoroutineContext
 
-class MatchListActivity : AppCompatActivity() {
+private const val SPLASHSCREEN_DURATION_MS = 1500L
+
+class MatchListActivity : AppCompatActivity(), CoroutineScope {
+
+    override val coroutineContext: CoroutineContext
+            by lazy { Dispatchers.Default + SupervisorJob() }
 
     private val viewModel: MatchListViewModel by viewModel()
     private val dateFormatter: DateFormatter by inject()
@@ -27,12 +35,26 @@ class MatchListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        configureSplashScreen()
+
         binding = ActivityMatchListBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         configureRecycler()
 
         viewModel.viewState.observe(this, ::onViewStateChanged)
+    }
+
+    private fun configureSplashScreen() {
+        val splashScreen = installSplashScreen()
+        var shouldDisplaySplash = true
+
+        splashScreen.setKeepOnScreenCondition { shouldDisplaySplash }
+
+        launch {
+            delay(SPLASHSCREEN_DURATION_MS)
+            shouldDisplaySplash = false
+        }
     }
 
     private fun configureRecycler() {
