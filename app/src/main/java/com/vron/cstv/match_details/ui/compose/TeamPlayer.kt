5b49e10b73d5e7cb480ff0eb67c8a42629857cd0
Victor Raft <victoraft@gmail.com>
2022-10-18 14:29:21 -0300

    Adjusting styles and sizes

--- app/src/main/java/com/vron/cstv/match_details/ui/compose/TeamPlayer.kt
+++ app/src/main/java/com/vron/cstv/match_details/ui/compose/TeamPlayer.kt
@@ -2,18 +2,23 @@ package com.vron.cstv.match_details.ui.compose
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
 import coil.compose.AsyncImage
@@ -21,9 +26,22 @@ import com.vron.cstv.R
 import com.vron.cstv.common.domain.fakes.buildFakePlayer
 import com.vron.cstv.common.domain.model.Player
 import com.vron.cstv.common.ui.compose.theme.CSTVTheme
-import com.vron.cstv.common.ui.compose.theme.CardShape
 import com.vron.cstv.common.ui.compose.theme.Gray
 import com.vron.cstv.common.ui.compose.theme.PlayerPictureShape
+
+private val LeftSidePlayerShape = RoundedCornerShape(
+    topStart = 0.dp,
+    topEnd = 16.dp,
+    bottomEnd = 16.dp,
+    bottomStart = 0.dp
+)
+
+private val RightSidePlayerShape = RoundedCornerShape(
+    topStart = 16.dp,
+    topEnd = 0.dp,
+    bottomEnd = 0.dp,
+    bottomStart = 16.dp
+)
 
 @Composable
 fun TeamPlayer(
@@ -39,9 +57,9 @@ fun TeamPlayer(
         val topMargin = dimensionResource(id = R.dimen.player_background_offset_top)
         Surface(
             color = MaterialTheme.colorScheme.primary,
-            shape = CardShape,
+            shape = if (isLeftSidePlayer) LeftSidePlayerShape else RightSidePlayerShape,
             modifier = Modifier.constrainAs(background) {
-                top.linkTo(parent.top, margin = topMargin)
+                top.linkTo(playerPicture.top, margin = topMargin)
                 bottom.linkTo(parent.bottom)
                 start.linkTo(parent.start)
                 end.linkTo(parent.end)
@@ -59,7 +77,6 @@ fun TeamPlayer(
                 .constrainAs(playerPicture) {
                     bottom.linkTo(background.bottom, pictureMarginBottom)
                     top.linkTo(parent.top, pictureMarginTop)
-                    top.linkTo(parent.top)
 
                     if (isLeftSidePlayer) {
                         end.linkTo(background.end, pictureMarginEnd)
@@ -73,34 +90,39 @@ fun TeamPlayer(
         Text(
             text = player.name,
             color = MaterialTheme.colorScheme.onSurface,
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Bold,
+            lineHeight = 16.sp,
             modifier = Modifier.constrainAs(playerNickname) {
-                width = Dimension.fillToConstraints
+                width = Dimension.preferredWrapContent
                 height = Dimension.wrapContent
 
-                bottom.linkTo(playerName.top)
-                top.linkTo(background.top)
+                linkTo(background.top, playerName.top, bias = 1f)
 
                 if (isLeftSidePlayer) {
-                    end.linkTo(playerPicture.start, margin = margin)
+                    linkTo(background.start, playerPicture.start, endMargin = margin, bias = 1f)
                 } else {
-                    start.linkTo(playerPicture.end, margin = margin)
+                    linkTo(playerPicture.end, background.end, startMargin = margin, bias = 0f)
                 }
             }
         )
 
         Text(
             text = "${player.firstName.trim()} ${player.lastName.trim()}",
-            color = MaterialTheme.colorScheme.onSurface,
+            color = Color(0xFF6C6B7E),
+            fontSize = 12.sp,
+            lineHeight = 14.sp,
+            textAlign = if (isLeftSidePlayer) TextAlign.End else TextAlign.Start,
             modifier = Modifier.constrainAs(playerName) {
-                width = Dimension.fillToConstraints
+                width = Dimension.preferredWrapContent
                 height = Dimension.wrapContent
 
                 bottom.linkTo(playerPicture.bottom)
 
                 if (isLeftSidePlayer) {
-                    end.linkTo(playerPicture.start, margin = margin)
+                    linkTo(background.start, playerPicture.start, startMargin = margin, endMargin = margin, bias = 1f)
                 } else {
-                    start.linkTo(playerPicture.end, margin = margin)
+                    linkTo(playerPicture.end, background.end, startMargin = margin, endMargin = margin, bias = 0f)
                 }
             }
         )
